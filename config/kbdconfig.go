cbf86b8e137f316066b7a1dfe4e534f2d18dface
jalenyang <jalen.yang@nokia-sbell.com>
2022-07-14 16:17:34 +0800

    fix the UT issue

--- config/kbdconfig.go
+++ config/kbdconfig.go
@@ -9,21 +9,22 @@ import (
 	"path"
 )
 
-var (
-	configFile string
-)
-
-func init() {
+func getConfigFile() string {
+	configPath := os.Getenv("KBD")
+	if configPath != "" {
+		return path.Join(configPath, "kbd.yaml")
+	}
 	home, err := os.UserHomeDir()
 	if err != nil {
 		log.Printf("failed to get the home dir %v", err)
 	}
-	configFile = path.Join(home, "kbd.yaml")
+	return path.Join(home, "kbd.yaml")
 }
 
 // LoadFromConfigFile parseConfig load the app config
 func LoadFromConfigFile() (module.KbdConfig, error) {
 	var kbdConfig module.KbdConfig
+	configFile := getConfigFile()
 	fi, err := os.OpenFile(configFile, os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
 		log.Panicln("Error: failed to open the config file 'kdb.yaml'")
@@ -43,6 +44,7 @@ func LoadFromConfigFile() (module.KbdConfig, error) {
 }
 
 func SyncToConfigFile(kbdConfig module.KbdConfig) error {
+	configFile := getConfigFile()
 	fi, err := os.OpenFile(configFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
 	if err != nil {
 		log.Panicln("Error: failed to open the config file 'kdb.yaml'")
--- config/kbdconfig_test.go
+++ config/kbdconfig_test.go
@@ -1,17 +1,41 @@
 package config
 
 import (
-	"kbd/realm"
+	"kbd/module"
+	"log"
+	"os"
 	"testing"
 )
 
-func TestLoadFromConfigFile(t *testing.T) {
-	want := KbdConfig{
+func TestMain(m *testing.M) {
+	setUP()
+	m.Run()
+	tearDown()
+}
+
+func setUP() {
+	os.Setenv("KBD", "../testdata")
+}
+
+func tearDown() {
+	os.Unsetenv("KBD")
+	kubeConfig := module.KbdConfig{
 		AppName:    "kbd",
 		AppVersion: "0.0.1",
-		Realms:     []realm.Realm{{Name: "kind-docker-desktop", Desc: "kind k8s cluster", KubeConfig: "config"}},
+		Realms:     []module.Realm{{Name: "kind-docker-desktop", Desc: "kind k8s cluster", KubeConfig: "config", Active: false}},
 	}
-	kbdConfig, err := LoadFromConfigFile("testdata/kbd.yaml")
+	if err := SyncToConfigFile(kubeConfig); err != nil {
+		log.Printf("teardown failed: %v", err)
+	}
+}
+
+func TestLoadFromConfigFile(t *testing.T) {
+	want := module.KbdConfig{
+		AppName:    "kbd",
+		AppVersion: "0.0.1",
+		Realms:     []module.Realm{{Name: "kind-docker-desktop", Desc: "kind k8s cluster", KubeConfig: "config", Active: false}},
+	}
+	kbdConfig, err := LoadFromConfigFile()
 
 	if err != nil {
 		t.FailNow()
@@ -23,27 +47,27 @@ func TestLoadFromConfigFile(t *testing.T) {
 		t.Errorf("kdb config %s != %s", want.AppVersion, kbdConfig.AppVersion)
 	}
 	if len(kbdConfig.Realms) == 0 || kbdConfig.Realms[0] != want.Realms[0] {
-		t.Errorf("kdb config %s != %s", want.Realms[0], kbdConfig.Realms[0])
+		t.Errorf("kdb config %v != %v", want.Realms[0], kbdConfig.Realms[0])
 	}
 }
 
 func TestSyncToConfigFile(t *testing.T) {
-	kConfig, err := LoadFromConfigFile("testdata/kbd.yaml")
+	kConfig, err := LoadFromConfigFile()
 	if err != nil {
 		t.FailNow()
 	}
 
-	kbdConfig.Realms = append(kConfig.Realms, realm.Realm{Name: "test", Desc: "desc", KubeConfig: "test_config"})
-	err = SyncToConfigFile("testdata/kbd.yaml")
+	kConfig.Realms = append(kConfig.Realms, module.Realm{Name: "test", Desc: "desc", KubeConfig: "test_config"})
+	err = SyncToConfigFile(kConfig)
 	if err != nil {
 		t.FailNow()
 	}
 
-	kConfig, err = LoadFromConfigFile("testdata/kbd.yaml")
+	kConfig, err = LoadFromConfigFile()
 	if err != nil {
 		t.FailNow()
 	}
-	if kConfig.Realms[1] != kbdConfig.Realms[1] {
-		t.Errorf("kdb config %v != %v", kConfig.Realms[1], kbdConfig.Realms[1])
+	if kConfig.Realms[1] != kConfig.Realms[1] {
+		t.Errorf("kdb config %v != %v", kConfig.Realms[1], kConfig.Realms[1])
 	}
 }
